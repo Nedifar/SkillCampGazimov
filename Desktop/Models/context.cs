8d989f5dcffdf91ee549a538e364e2356a11a3c5
Ильназ Газимов <ilnaz.gazimov@mail.ru>
2022-03-11 11:41:15 +0500

    Добавьте файлы проекта.

---
+++ Desktop/Models/context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Models
+{
+    class context : DbContext
+    {
+        private static context _context;
+        public context() : base("sql") { }
+        public static context aGetContext()
+        {
+            if (_context == null)
+                _context = new context();
+            return _context;
+        }
+    }
+}
